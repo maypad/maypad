0b65675ac2de3bdd411dd4c303ae5ab5caa91ca5
Julian Todt <code@j-todt.de>
2018-12-21 16:49:17 +0100

    Add checkstyle

--- backend/src/main/java/de/fraunhofer/iosb/maypadbackend/MaypadBackendApplication.java
+++ backend/src/main/java/de/fraunhofer/iosb/maypadbackend/MaypadBackendApplication.java
@@ -6,9 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MaypadBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MaypadBackendApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(MaypadBackendApplication.class, args);
+  }
 
 }
-
