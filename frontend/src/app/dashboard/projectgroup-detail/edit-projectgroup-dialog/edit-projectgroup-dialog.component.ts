9d2f9ec175f33fd38b96457eec9381da15dd28f4
Cranken <cranken1337@gmail.com>
2019-01-05 16:54:21 +0100

    Add edit projectgroup dialog. Add delete modal.

--- frontend/src/app/dashboard/projectgroup-detail/edit-projectgroup-dialog/edit-projectgroup-dialog.component.ts
+++ frontend/src/app/dashboard/projectgroup-detail/edit-projectgroup-dialog/edit-projectgroup-dialog.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
+import { Projectgroup } from 'src/app/model/projectGroup';
 
 @Component({
   selector: 'app-edit-projectgroup-dialog',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-projectgroup-dialog.component.css']
 })
 export class EditProjectgroupDialogComponent implements OnInit {
-
+  @Input('projGroup') projGroup: Projectgroup;
+  @ViewChild('newNameInput') newNameInput: ElementRef;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  clearInput() {
+    this.newNameInput.nativeElement.value = '';
+  }
+
+  updateProjectgroup(newName: string) {
+    console.log(`Update projgroup ${this.projGroup.id} to new name: ${newName}`);
+    // Reload page?
+  }
+
+  deleteProjectgroup() {
+    console.log(`Delete projgroup ${this.projGroup.id}.`);
+    // Reload page?
   }
 
 }
--- frontend/src/app/dashboard/projectgroup-detail/projectgroup-detail.component.ts
+++ frontend/src/app/dashboard/projectgroup-detail/projectgroup-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { Projectgroup } from 'src/app/model/projectGroup';
 import { AddProjectDialogComponent } from './add-project-dialog/add-project-dialog.component';
+import { EditProjectgroupDialogComponent } from './edit-projectgroup-dialog/edit-projectgroup-dialog.component';
 
 @Component({
   selector: 'app-projectgroup-detail',
@@ -10,6 +11,7 @@ import { AddProjectDialogComponent } from './add-project-dialog/add-project-dial
 export class ProjectgroupDetailComponent implements OnInit {
   @Input('projGroup') projGroup: Projectgroup;
   @ViewChild('addProjectDialog{{projGroup.id}}') addModal: AddProjectDialogComponent;
+  @ViewChild('editProjectgroupDialog{{projGroup.id}}') editModal: EditProjectgroupDialogComponent;
   constructor() { }
 
   ngOnInit() {
@@ -18,6 +20,7 @@ export class ProjectgroupDetailComponent implements OnInit {
   clearInput(event: FocusEvent) {
     if (event.relatedTarget == null) {
       this.addModal.clearInput();
+      this.editModal.clearInput();
     }
   }
 }
