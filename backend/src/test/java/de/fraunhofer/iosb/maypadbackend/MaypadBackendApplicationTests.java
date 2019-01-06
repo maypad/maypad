f90bc52e9bb62a0cab97e27be03949dec6d50721
Julian Todt <code@j-todt.de>
2019-01-06 21:29:55 +0100

    Change indentation in backend (#8)
    * Fix checkstyle to use 4 spaces as indent
    
    * Fix checkstyle violations

--- backend/src/test/java/de/fraunhofer/iosb/maypadbackend/MaypadBackendApplicationTests.java
+++ backend/src/test/java/de/fraunhofer/iosb/maypadbackend/MaypadBackendApplicationTests.java
@@ -9,9 +9,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class MaypadBackendApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+
+    }
 
 }
 
