ddceb92c2ef54ece5b768d919bde0c47b4752f18
Max Willich <maxwillich99@gmail.com>
2019-01-07 15:20:11 +0100

    Add implementation for Webconfig, ServerConfig; add empty implentation to DatabaseConfig
    - Basic ServerConfig-implementation for storing properties
    - WebConfig implementation with Spring Beans for setting locale with "?lang="-Intercepts.
    - Empty DatabaseConfig because design questions still have to be resolved.

--- backend/src/main/java/de/fraunhofer/iosb/maypadbackend/config/DatabaseConfig.java
+++ backend/src/main/java/de/fraunhofer/iosb/maypadbackend/config/DatabaseConfig.java
@@ -1,10 +1,27 @@
 package de.fraunhofer.iosb.maypadbackend.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
 /**
  *  Configuration class to establish a connection to a database, which is used by
  *  Maypad, e.g. for the Java Persistence API.
  *
  * @author Max Willich
  */
+@Configuration
 public class DatabaseConfig {
+
+    @Bean
+    public DataSource getDataSource() {
+        return null;
+    }
+
+    @Bean
+    public Properties getAdditionalProperties() {
+        return null;
+    }
 }
--- backend/src/main/java/de/fraunhofer/iosb/maypadbackend/config/WebConfig.java
+++ backend/src/main/java/de/fraunhofer/iosb/maypadbackend/config/WebConfig.java
@@ -1,10 +1,59 @@
 package de.fraunhofer.iosb.maypadbackend.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
+
 /**
  *  Configuration Class to access properties for the client, e.g. the locale.
  *
  * @author Max Willich
  */
-public class WebConfig {
+@ComponentScan
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
 
+    /**
+     * Returns LocaleResolver that resolves current locale from session.
+     * If locale can't be resolved, US-Locale is set as default.
+     *
+     * @return LocaleResolver with properties mentioned above.
+     */
+    @Bean
+    public LocaleResolver getLocaleResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.US);
+        return slr;
+    }
+
+    /**
+     * Returns a LocaleChangeInterceptor which is set up to trigger when "?lang=..." is present
+     * in a request.
+     *
+     * @return The Interceptor Spring will use to change the locale
+     */
+    @Bean
+    public LocaleChangeInterceptor getLocaleChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    /**
+     * Implements addInterceptors from WebMvcConfigurer, adds a LocaleChangeInterceptor
+     * with correct setup to the spring-application (see getLocaleChangeInterceptor()).
+     *
+     * @param registry Registry in which the interceptor is supposed to be added.
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(getLocaleChangeInterceptor());
+    }
 }
