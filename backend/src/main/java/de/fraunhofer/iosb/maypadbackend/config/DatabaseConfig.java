3e23fd3aeabc89df16ee3d6805935e3c0dd629e6
freak532486 <maxwillich99@gmail.com>
2019-01-09 22:44:20 +0100

    Add implementation for DatabaseConfig
    - Added implementation for getDataSource() in DatabaseConfig
    - Deleted getAdditionalProperties() (redundant because of ServerConfig-Class)
    - Added Mysql-Dependency to pom.xml

--- backend/src/main/java/de/fraunhofer/iosb/maypadbackend/config/DatabaseConfig.java
+++ backend/src/main/java/de/fraunhofer/iosb/maypadbackend/config/DatabaseConfig.java
@@ -1,10 +1,11 @@
 package de.fraunhofer.iosb.maypadbackend.config;
 
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
-import java.util.Properties;
 
 /**
  *  Configuration class to establish a connection to a database, which is used by
@@ -15,13 +16,31 @@ import java.util.Properties;
 @Configuration
 public class DatabaseConfig {
 
+    @Value("${database.user}")
+    private String user;
+    @Value("${database.password}")
+    private String password;
+    @Value("${database.database}")
+    private String database;
+    @Value("${database.host}")
+    private String host;
+    @Value("${database.port}")
+    private int port;
+
+    /**
+     * Creates a DataSource-Object to access the Maypad database with
+     * the connection details specified in the application.properties file.
+     *
+     * @return The DataSource-Object for connection purposes.
+     */
     @Bean
     public DataSource getDataSource() {
-        return null;
-    }
-
-    @Bean
-    public Properties getAdditionalProperties() {
-        return null;
+        MysqlDataSource ret = new MysqlDataSource();
+        ret.setUser(user);
+        ret.setPassword(password);
+        ret.setDatabaseName(database);
+        ret.setUrl(host);
+        ret.setPort(port);
+        return ret;
     }
 }
