d190d9b24cc98a15626d069614c9905004d5e09a
Cranken <cranken1337@gmail.com>
2019-01-16 00:42:58 +0100

    Add unit tests for components.

--- frontend/src/app/project-detail/project-branch-resolver.service.spec.ts
+++ frontend/src/app/project-detail/project-branch-resolver.service.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ProjectBranchResolverService } from './project-branch-resolver.service';
+import { ProjectService } from '../project.service';
+import { ProjectServiceStub } from 'src/testing/project-service-stub';
 
 describe('ProjectBranchResolverService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [{ provide: ProjectService, useClass: ProjectServiceStub }]
+  }));
 
   it('should be created', () => {
     const service: ProjectBranchResolverService = TestBed.get(ProjectBranchResolverService);
