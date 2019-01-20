7f3762c3cf20878c71ddfecfe59d00130e834a42
Cranken <cranken1337@gmail.com>
2019-01-20 15:43:04 +0100

    Fix & add unit test.

--- frontend/src/app/build-history/bhistory-list-item/bhistory-list-item.component.spec.ts
+++ frontend/src/app/build-history/bhistory-list-item/bhistory-list-item.component.spec.ts
@@ -1,25 +1,36 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BhistoryListItemComponent } from './bhistory-list-item.component';
+import * as buildsResponse from 'sample-requests/get.projects.id.branches.ref.builds.response.json';
+import { Build } from 'src/app/model/build';
 
 describe('BhistoryListItemComponent', () => {
   let component: BhistoryListItemComponent;
   let fixture: ComponentFixture<BhistoryListItemComponent>;
+  const build: Build = buildsResponse['default'][0];
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BhistoryListItemComponent ]
+      declarations: [BhistoryListItemComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(BhistoryListItemComponent);
     component = fixture.componentInstance;
+    component.build = build;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should list build info', () => {
+    const nativeElement: HTMLElement = fixture.nativeElement;
+    const elements: NodeListOf<Element> = nativeElement.querySelectorAll('span');
+
+    expect(elements[3].textContent).toBe('Commit: <' + build.commit.hash.substr(0, 7) + '>');
   });
 });
