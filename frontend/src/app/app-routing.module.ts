bd49716cb7e066b413c6ac8c6454a25a5a5b904c
Cranken <cranken1337@gmail.com>
2019-01-21 15:02:30 +0100

    Fix 404 not appearing on non existing project. Reworked 404 site. Add getting projectname of branch from service.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -24,7 +24,7 @@ const routes: Routes = [
   },
   {
     path: 'projects/:id/branches/:branch', component: BranchDetailComponent,
-    resolve: { branch: BranchDetailResolverService }
+    resolve: { branch: BranchDetailResolverService, project: ProjectDetailResolverService }
   },
   {
     path: 'projects/:id', component: ProjectDetailComponent,
--- frontend/src/app/branch-detail/branch-detail.component.ts
+++ frontend/src/app/branch-detail/branch-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { BreadcrumbService } from '../breadcrumb.service';
 import { Branch } from '../model/branch';
+import { Project } from '../model/project';
 
 @Component({
   selector: 'app-branch-detail',
@@ -10,17 +11,17 @@ import { Branch } from '../model/branch';
 })
 export class BranchDetailComponent implements OnInit {
   branch: Branch;
-  projId: number;
+  project: Project;
   constructor(private route: ActivatedRoute, private crumbs: BreadcrumbService) { }
 
   ngOnInit() {
-    this.projId = parseInt(this.route.snapshot.paramMap.get('id'), 10);
-    this.route.data.subscribe((data: { branch: Branch }) => {
+    this.route.data.subscribe((data: { branch: Branch, project: Project }) => {
       this.branch = data.branch;
+      this.project = data.project;
+      this.crumbs.setBreadcrumbs([
+        { name: this.project.name, path: '/projects/' + this.project.id },
+        { name: this.branch.name, path: '/projects/' + this.project.id + '/branches/' + this.branch.name }
+      ]);
     });
-    this.crumbs.setBreadcrumbs([
-      { name: this.branch.projectName, path: '/projects/' + this.projId },
-      { name: this.branch.name, path: '/projects/' + this.projId + '/branches/' + this.branch.name }
-    ]);
   }
 }
