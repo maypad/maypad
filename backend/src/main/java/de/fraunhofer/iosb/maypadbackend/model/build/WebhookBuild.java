57aa0f5ed8de08a6dd1d88f117769a4ef5c68f66
Az0r <15131687+Az0r@users.noreply.github.com>
2019-01-23 18:04:00 +0100

    Fix ProjectMapper unit test, Fix checkstyle violations

--- backend/src/main/java/de/fraunhofer/iosb/maypadbackend/model/build/WebhookBuild.java
+++ backend/src/main/java/de/fraunhofer/iosb/maypadbackend/model/build/WebhookBuild.java
@@ -1,9 +1,9 @@
 package de.fraunhofer.iosb.maypadbackend.model.build;
 
 import de.fraunhofer.iosb.maypadbackend.model.webhook.ExternalWebhook;
-import lombok.AllArgsConstructor;
 import de.fraunhofer.iosb.maypadbackend.services.build.BuildTypeExec;
 import de.fraunhofer.iosb.maypadbackend.services.build.WebhookBuildExecutor;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
