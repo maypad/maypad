c883a12564116a387e745cfe20285b8d02cad2ee
Lukas Brosch <lukas.brosch@web.de>
2019-01-23 03:34:42 +0100

    Add utility for files and tuple

---
+++ backend/src/main/java/de/fraunhofer/iosb/maypadbackend/util/FileUtil.java
@@ -0,0 +1,117 @@
+package de.fraunhofer.iosb.maypadbackend.util;
+
+import com.google.common.base.Charsets;
+import org.apache.tomcat.util.http.fileupload.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.xml.bind.DatatypeConverter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import static com.google.common.io.Files.asCharSource;
+
+/**
+ * Utils for file and directory operations.
+ *
+ * @author Lukas Brosch
+ * @version 1.0
+ */
+public class FileUtil {
+
+    private static Logger logger = LoggerFactory.getLogger(FileUtil.class);
+
+    /**
+     * Check whether write and read access exists.
+     *
+     * @param file File to check
+     * @return true if write and read access exists, else false
+     */
+    public static boolean hasWriteAccess(File file) {
+        return hasReadAccess(file) && file.canWrite();
+    }
+
+    /**
+     * Delete all files at a location.
+     *
+     * @param file Location for deleting recursively
+     * @return true if files could be deleted, else false
+     */
+    public static boolean deleteAllFiles(File file) {
+        try {
+            FileUtils.deleteDirectory(file);
+        } catch (IOException e) {
+            logger.error("Can't delete files at: " + file.getAbsoluteFile());
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Calc a SHA-256 hashvalue (checksum) from a file.
+     *
+     * @param input File
+     * @return Checksum of the file
+     */
+    public static String calcSha256(File input) {
+        try (InputStream in = new FileInputStream(input)) {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] block = new byte[4096];
+            int length;
+            while ((length = in.read(block)) > 0) {
+                digest.update(block, 0, length);
+            }
+            return DatatypeConverter.printHexBinary(digest.digest());
+        } catch (NoSuchAlgorithmException | IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * Check whether the given file exists and the current user can read the file.
+     *
+     * @param file File to check
+     * @return true, if the user has access to read, else false
+     */
+    public static boolean hasReadAccess(File file) {
+        if (file == null || !file.exists()) {
+            return false;
+        }
+        return file.canRead();
+    }
+
+    /**
+     * Check whether the given file exists, the current user can read the file and whether the file is a file.
+     *
+     * @param file File to check
+     * @return true, if the user has access to read, else false
+     */
+    public static boolean canReadFile(File file) {
+        return hasReadAccess(file) && file.isFile();
+    }
+
+    /**
+     * Get the content of a file.
+     *
+     * @param file File
+     * @return Content of the file
+     */
+    public static String getFileContent(File file) {
+        if (!canReadFile(file)) {
+            return null;
+        }
+        try {
+            return asCharSource(file, Charsets.UTF_8).read();
+        } catch (IOException e) {
+            logger.warn("Can't read file: " + file.getAbsoluteFile());
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
---
+++ backend/src/main/java/de/fraunhofer/iosb/maypadbackend/util/Tuple.java
@@ -0,0 +1,19 @@
+package de.fraunhofer.iosb.maypadbackend.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * A Tuple.
+ *
+ * @author Lukas Brosch
+ * @version 1.0
+ */
+@Data
+@AllArgsConstructor
+public class Tuple<K, V> {
+    private K key;
+    private V value;
+}
+
+
