1ee8bed383e5da672fb91f51ee1912094f9fd613
freak532486 <maxwillich99@gmail.com>
2019-01-24 15:57:50 +0100

    Implement SvnRepoManager
    Notes:
    - Tags are not attached to commits in SVN. Tag.commit = null for now.
    
    - Getting tag and branch names is HIGHLY inefficient, because a tag and
      a branch is just a folder in SVN. Have to list all folders in the branches/tags folder.
    
    - SVN Repo has to be structured like this:
    
      root
      |-- trunk
      |-- branches
      |   |-- branch1
      |   |-- branch2
      |
      |-- tags
      |   |-- 1.0.0-RELEASE
    
      This is hardcoded for now. Changes might me made later.
    
    - This is not tested at all yet.

--- backend/src/main/java/de/fraunhofer/iosb/maypadbackend/services/reporefresh/SvnRepoManager.java
+++ backend/src/main/java/de/fraunhofer/iosb/maypadbackend/services/reporefresh/SvnRepoManager.java
@@ -1,9 +1,23 @@
 package de.fraunhofer.iosb.maypadbackend.services.reporefresh;
 
+import de.fraunhofer.iosb.maypadbackend.config.server.YamlServerConfig;
 import de.fraunhofer.iosb.maypadbackend.model.Project;
+import de.fraunhofer.iosb.maypadbackend.model.person.Author;
+import de.fraunhofer.iosb.maypadbackend.model.repository.Branch;
 import de.fraunhofer.iosb.maypadbackend.model.repository.Commit;
 import de.fraunhofer.iosb.maypadbackend.model.repository.Tag;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.tmatesoft.svn.core.ISVNLogEntryHandler;
+import org.tmatesoft.svn.core.SVNDepth;
+import org.tmatesoft.svn.core.SVNException;
+import org.tmatesoft.svn.core.SVNLogEntry;
+import org.tmatesoft.svn.core.SVNURL;
+import org.tmatesoft.svn.core.wc.SVNClientManager;
+import org.tmatesoft.svn.core.wc.SVNRevision;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -13,6 +27,11 @@ import java.util.List;
  * @version 1.0
  */
 public class SvnRepoManager extends RepoManager {
+
+    private SVNClientManager svnClientManager = SVNClientManager.newInstance();
+    private Logger logger = LoggerFactory.getLogger(SvnRepoManager.class);
+
+    private String projectRoot;
 
     /**
      * Constructor, prepare the SvnRepoManager.
@@ -31,6 +50,7 @@ public class SvnRepoManager extends RepoManager {
      */
     public SvnRepoManager(Project project) {
         super(project);
+        projectRoot = project.getRepository().getRootFolder().getAbsolutePath();
     }
 
     /**
@@ -40,7 +60,15 @@ public class SvnRepoManager extends RepoManager {
      */
     @Override
     public List<String> getBranchNames() {
-        return null;
+        File branchesFolder = new File(projectRoot + "/branches/");
+        String[] branchList = branchesFolder.list();
+        List<String> branches = new ArrayList<String>();
+        for (String b : branchList) {
+            if (new File(b).isDirectory()) {
+                branches.add(new File(b).getName());
+            }
+        }
+        return branches;
     }
 
     /**
@@ -51,7 +79,19 @@ public class SvnRepoManager extends RepoManager {
      */
     @Override
     public boolean switchBranch(String name) {
-        return true;
+        if (name.equals("trunk")) {
+            this.getProject().getRepository().setRootFolder(new File(projectRoot + "/trunk/"));
+            return true;
+        } else {
+            File branchFolder = new File(projectRoot + "/branches/" + name);
+            if (!branchFolder.isDirectory() || !branchFolder.exists()) {
+                logger.error("Branch of name " + name + " doesn't exist");
+                logger.error("Not found in directory " + branchFolder.getAbsolutePath());
+                return false;
+            }
+            this.getProject().getRepository().setRootFolder(branchFolder);
+            return true;
+        }
     }
 
 
@@ -62,7 +102,18 @@ public class SvnRepoManager extends RepoManager {
      */
     @Override
     public List<Tag> getTags() {
-        return null;
+        File tagFolder = new File(
+                this.getProject().getRepository().getRootFolder().getAbsolutePath() + "/tags/"
+        );
+        String[] tagList = tagFolder.list();
+        List<Tag> tags = new ArrayList<Tag>();
+        for (String t : tagList) {
+            if (new File(t).isDirectory()) {
+                Tag _t = new Tag();
+                _t.setName(new File(t).getName());
+            }
+        }
+        return tags;
     }
 
     /**
@@ -72,7 +123,22 @@ public class SvnRepoManager extends RepoManager {
      */
     @Override
     public Commit getLastCommit() {
-        return null;
+        try {
+            RecentCommitHandler rcm = new RecentCommitHandler();
+            svnClientManager.getLogClient().doLog(
+                    new File[] { this.getProject().getRepository().getRootFolder() },
+                    SVNRevision.HEAD,
+                    SVNRevision.HEAD,
+                    true,
+                    false,
+                    1,
+                    rcm
+            );
+            return rcm.getCommit();
+        } catch (SVNException ex) {
+            logger.error(ex.getMessage());
+            return null;
+        }
     }
 
     /**
@@ -82,7 +148,38 @@ public class SvnRepoManager extends RepoManager {
      */
     @Override
     protected boolean cloneRepository() {
-        return true;
+        try {
+            SVNURL url = SVNURL.parseURIEncoded(this.getProject().getRepositoryUrl());
+            svnClientManager.getUpdateClient().doCheckout(
+                    url, // Clone URL
+                    this.getProject().getRepository().getRootFolder(), // Folder to clone into
+                    SVNRevision.HEAD,
+                    SVNRevision.HEAD, // What revision to clone (head == latest revision)
+                    SVNDepth.INFINITY, // How far to clone the history tree
+                    true
+            );
+            return true;
+        } catch (SVNException ex) {
+            logger.error(ex.getMessage());
+            return false;
+        }
+    }
+
+    private class RecentCommitHandler implements ISVNLogEntryHandler {
+
+        private Commit commit;
+
+        @Override
+        public void handleLogEntry(SVNLogEntry svnLogEntry) throws SVNException {
+            commit.setAuthor(new Author(svnLogEntry.getAuthor(), null));
+            commit.setTimestamp(svnLogEntry.getDate());
+            commit.setCommitIdentifier(null); // No SVN commit identifier
+            commit.setCommitMessage(svnLogEntry.getMessage());
+        }
+
+        public Commit getCommit() {
+            return commit;
+        }
     }
 
 }
