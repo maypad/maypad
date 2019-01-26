051f77b4c24134edeb1892834fb18ffe12f24884
Lukas Brosch <lukas.brosch@web.de>
2019-01-27 00:06:10 +0100

    Change branch repository

--- backend/src/main/java/de/fraunhofer/iosb/maypadbackend/repositories/BranchRepository.java
+++ backend/src/main/java/de/fraunhofer/iosb/maypadbackend/repositories/BranchRepository.java
@@ -4,7 +4,14 @@ import de.fraunhofer.iosb.maypadbackend.model.repository.Branch;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface BranchRepository extends JpaRepository<Branch, Integer> {
+
+    public List<Branch> findAll();
+
+    public Optional<Branch> findBranchById(Integer id);
 
 }
