6d3e1d43dbc217f9b20503fff262a18fd359aaf9
freak532486 <maxwillich99@gmail.com>
2019-01-27 20:35:33 +0100

    Various fixes and improvements for SvnRepoManager.
    - SvnRepoManager is a singleton now (because projectRoot needs to be persisted)
    - YamlProjectConfig returns empty list instead of null when no branches are specified.
    - Other minor fixes.

--- backend/src/main/java/de/fraunhofer/iosb/maypadbackend/config/project/YamlProjectConfig.java
+++ backend/src/main/java/de/fraunhofer/iosb/maypadbackend/config/project/YamlProjectConfig.java
@@ -11,6 +11,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -77,7 +78,9 @@ public class YamlProjectConfig implements ProjectConfig {
      */
     @Override
     public List<BranchProperty> getBranchProperties() {
-        return yamlObject.getBranches();
+
+        List<BranchProperty> branches = yamlObject.getBranches();
+        return (branches == null) ? new ArrayList<BranchProperty>() : branches;
     }
 
     @Override
