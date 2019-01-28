8f0343f9d2f09aee581bbd04e8179bda32174479
Az0r <15131687+Az0r@users.noreply.github.com>
2019-01-29 00:06:13 +0100

    Implement Scheduler startup, Use config values

--- backend/src/main/java/de/fraunhofer/iosb/maypadbackend/services/scheduler/RefreshTask.java
+++ backend/src/main/java/de/fraunhofer/iosb/maypadbackend/services/scheduler/RefreshTask.java
@@ -1,19 +1,31 @@
 package de.fraunhofer.iosb.maypadbackend.services.scheduler;
 
-import de.fraunhofer.iosb.maypadbackend.model.Project;
+import de.fraunhofer.iosb.maypadbackend.services.reporefresh.RepoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Runnable that refreshes the given Project.
- * TODO:
- * - Add RepoService Attribute
- * - Implement run
  */
 public class RefreshTask implements Runnable {
-    private Project project;
+    private int projectId;
+    private RepoService repoService;
 
+    private static final Logger logger = LoggerFactory.getLogger(RefreshTask.class);
+
+    /**
+     * Constructor for RefreshTask.
+     * @param projectId the id of the project that should be updated
+     * @param repoService the RepoService used to refresh a repository
+     */
+    public RefreshTask(int projectId, RepoService repoService) {
+        this.projectId = projectId;
+        this.repoService = repoService;
+    }
 
     @Override
     public void run() {
-        //Implementation requires RepoService
+        logger.debug("Running scheduled task for project " + projectId);
+        repoService.refreshProject(projectId);
     }
 }
--- backend/src/main/java/de/fraunhofer/iosb/maypadbackend/services/scheduler/SchedulerService.java
+++ backend/src/main/java/de/fraunhofer/iosb/maypadbackend/services/scheduler/SchedulerService.java
@@ -1,9 +1,16 @@
 package de.fraunhofer.iosb.maypadbackend.services.scheduler;
 
+import de.fraunhofer.iosb.maypadbackend.config.server.ServerConfig;
 import de.fraunhofer.iosb.maypadbackend.model.Project;
+import de.fraunhofer.iosb.maypadbackend.repositories.ProjectRepository;
+import de.fraunhofer.iosb.maypadbackend.services.reporefresh.RepoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ScheduledFuture;
@@ -12,43 +19,61 @@ import java.util.concurrent.ScheduledFuture;
 public class SchedulerService {
     private ThreadPoolTaskScheduler threadPoolTaskScheduler;
     private Map<Integer, ScheduledFuture<?>> taskMapping;
+    private ServerConfig serverConfig;
+    private RepoService repoService;
+    private ProjectRepository projectRepository;
 
-    private static final int poolSize = 4; //read from config
-    private static final long refreshInterval = 10000; //read from config
+    private static final Logger logger = LoggerFactory.getLogger(SchedulerService.class);
 
     /**
      * Initializes the threadPoolTaskScheduler and the taskMapping.
+     * @param serverConfig the serverconfig
+     * @param projectRepository the ProjectRepository used to access project
+     * @param repoService the RepoService used to update repositories
      */
-    public SchedulerService() {
+    @Autowired
+    public SchedulerService(ServerConfig serverConfig, RepoService repoService, ProjectRepository projectRepository) {
+        this.serverConfig = serverConfig;
+        this.repoService = repoService;
+        this.projectRepository = projectRepository;
         threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
         taskMapping = new HashMap<>();
-        threadPoolTaskScheduler.setPoolSize(poolSize);
+        threadPoolTaskScheduler.setPoolSize(serverConfig.getSchedulerPoolSize());
         threadPoolTaskScheduler.setRemoveOnCancelPolicy(true);
         threadPoolTaskScheduler.initialize();
     }
 
     /**
      * Adds a project to the scheduling.
-     * @param project the project that should be refreshed in the set interval.
+     * @param projectId the id of the project that should be updated
      */
-    public void scheduleRepoRefresh(Project project) {
-        if (!taskMapping.containsKey(project.getId())) {
-            RefreshTask task = new RefreshTask();
+    public void scheduleRepoRefresh(int projectId) {
+        if (!taskMapping.containsKey(projectId)) {
+            logger.info("Scheduled Project " + projectId);
+            RefreshTask task = new RefreshTask(projectId, repoService);
             ScheduledFuture<?> sf = threadPoolTaskScheduler.scheduleWithFixedDelay(task,
-                    refreshInterval);
-            taskMapping.put(project.getId(), sf);
+                    serverConfig.getReloadRepositoriesSeconds());
+            taskMapping.put(projectId, sf);
         }
     }
 
     /**
      * Removes a project from the scheduling.
-     * @param project the project that should be removed from the scheduling
+     * @param projectId the id of the project that should be unscheduled
      */
-    public void unscheduleRepoRefresh(Project project) {
-        if (taskMapping.containsKey(project.getId())) {
-            ScheduledFuture<?> sf = taskMapping.get(project.getId());
+    public void unscheduleRepoRefresh(int projectId) {
+        if (taskMapping.containsKey(projectId)) {
+            logger.info("Unscheduled Project " + projectId);
+            ScheduledFuture<?> sf = taskMapping.get(projectId);
             sf.cancel(false);
-            taskMapping.remove(project.getId());
+            taskMapping.remove(projectId);
+        }
+    }
+
+    @PostConstruct
+    private void init() {
+        for (Project project : projectRepository.findAll()) {
+            scheduleRepoRefresh(project.getId());
         }
     }
 }
