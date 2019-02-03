de851b520f26f333e7222f733c0a7c594e27dda4
Lukas Brosch <lukas.brosch@web.de>
2019-02-03 03:12:17 +0100

    Add refresh cap and fixes for services

--- backend/src/main/java/de/fraunhofer/iosb/maypadbackend/services/reporefresh/RepoService.java
+++ backend/src/main/java/de/fraunhofer/iosb/maypadbackend/services/reporefresh/RepoService.java
@@ -36,6 +36,7 @@ import org.springframework.stereotype.Service;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Date;
