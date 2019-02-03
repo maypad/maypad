8097e6541a6183194ac051def2281887ed108047
Lukas Brosch <lukas.brosch@web.de>
2019-02-03 03:06:40 +0100

    Add expiring datastructure

---
+++ backend/src/main/java/de/fraunhofer/iosb/maypadbackend/util/datastructures/ExpiringElement.java
@@ -0,0 +1,34 @@
+package de.fraunhofer.iosb.maypadbackend.util.datastructures;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * A general element with TTL.
+ *
+ * @author Lukas Brosch
+ * @version 1.0
+ */
+public class ExpiringElement {
+
+    private final long expirationTimestamp;
+
+    /**
+     * Constructor for ExpiringElement.
+     *
+     * @param ttl      TTL
+     * @param timeUnit timeunit for ttl
+     */
+    public ExpiringElement(long ttl, TimeUnit timeUnit) {
+        expirationTimestamp = System.currentTimeMillis() + timeUnit.toMillis(ttl);
+    }
+
+    /**
+     * Check, if this element expired.
+     *
+     * @return true, if expired, else false
+     */
+    public boolean isExpired() {
+        return System.currentTimeMillis() > expirationTimestamp;
+    }
+
+}
