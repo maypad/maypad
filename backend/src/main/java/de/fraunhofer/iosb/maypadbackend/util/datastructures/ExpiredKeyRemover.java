80a9a291925e9ede3994adae5c3beb10afb399d7
Lukas Brosch <lukas.brosch@web.de>
2019-02-03 03:06:40 +0100

    Add expiring datastructure

---
+++ backend/src/main/java/de/fraunhofer/iosb/maypadbackend/util/datastructures/ExpiredKeyRemover.java
@@ -0,0 +1,23 @@
+package de.fraunhofer.iosb.maypadbackend.util.datastructures;
+
+import lombok.AllArgsConstructor;
+
+import java.util.List;
+
+/**
+ * Remove an entry of the map, if ttl expired.
+ *
+ * @author Lukas Brosch
+ * @version 1.0
+ */
+@AllArgsConstructor
+public class ExpiredKeyRemover implements Runnable {
+
+    private final List<ExpiringElement> elements;
+
+    @Override
+    public void run() {
+        elements.removeIf(ExpiringElement::isExpired);
+    }
+
+}
