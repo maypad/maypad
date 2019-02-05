42a1e927a50d76df63d7929a1762868812ffc722
Lukas Brosch <lukas.brosch@web.de>
2019-02-05 22:45:06 +0100

    Removed not needed import

--- backend/src/main/java/de/fraunhofer/iosb/maypadbackend/services/reporefresh/RepoService.java
+++ backend/src/main/java/de/fraunhofer/iosb/maypadbackend/services/reporefresh/RepoService.java
@@ -35,7 +35,6 @@ import org.springframework.stereotype.Service;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Date;
