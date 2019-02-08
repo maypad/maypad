3c77c4ce1a100ba8fd59139f49760828762e0033
Az0r <15131687+Az0r@users.noreply.github.com>
2019-02-08 18:29:08 +0100

    Add missing javadoc for event types

--- backend/src/main/java/de/fraunhofer/iosb/maypadbackend/services/sse/SseEventType.java
+++ backend/src/main/java/de/fraunhofer/iosb/maypadbackend/services/sse/SseEventType.java
@@ -5,14 +5,29 @@ package de.fraunhofer.iosb.maypadbackend.services.sse;
  */
 public enum SseEventType {
 
+    /**
+     * Thrown when a project was initialized.
+     */
     PROJECT_INIT("project_init"),
 
+    /**
+     * Thrown when a project was refreshed.
+     */
     PROJECT_REFRESHED("project_refreshed"),
 
+    /**
+     * Thrown when the authentication method was updated.
+     */
     AUTHMETHOD_UPDATED("auth_updated"),
 
+    /**
+     * Thrown when a build status was updated.
+     */
     BUILD_UPDATE("build_updated"),
 
+    /**
+     * Thrown when a deployment status was updated.
+     */
     DEPLOYMENT_UPDATE("deployment_updated");
 
     private String eventId;
@@ -21,6 +36,11 @@ public enum SseEventType {
         this.eventId = eventId;
     }
 
+    /**
+     * Return the event id.
+     *
+     * @return the event id
+     */
     public String getEventId() {
         return eventId;
     }
