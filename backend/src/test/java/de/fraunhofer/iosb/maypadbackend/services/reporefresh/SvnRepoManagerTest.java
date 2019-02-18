728cb2ac904d427217be1c49b26e327ce2234998
freak532486 <maxwillich99@gmail.com>
2019-02-18 12:24:11 +0100

    Replace DI with mock project for actual unit testing

--- backend/src/test/java/de/fraunhofer/iosb/maypadbackend/services/reporefresh/SvnRepoManagerTest.java
+++ backend/src/test/java/de/fraunhofer/iosb/maypadbackend/services/reporefresh/SvnRepoManagerTest.java
@@ -4,21 +4,18 @@ import de.fraunhofer.iosb.maypadbackend.config.project.ProjectConfig;
 import de.fraunhofer.iosb.maypadbackend.model.Project;
 import de.fraunhofer.iosb.maypadbackend.model.repository.Commit;
 import de.fraunhofer.iosb.maypadbackend.model.repository.Tag;
-import de.fraunhofer.iosb.maypadbackend.services.ProjectService;
-import de.fraunhofer.iosb.maypadbackend.services.ProjectgroupService;
 import de.fraunhofer.iosb.maypadbackend.util.Tuple;
 import org.apache.commons.collections4.CollectionUtils;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -29,22 +26,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@RunWith(MockitoJUnitRunner.class)
 public class SvnRepoManagerTest {
 
-    @Value("classpath:testrepo_svn")
-    private Resource repositoryResource;
-
     private static final Logger logger = LoggerFactory.getLogger(SpringRunner.class);
-    private File repositoryFile;
-    private Project project;
     private SvnRepoManager svnRepoManager;
-    private int pgid;
 
     private List<String> correctBranchList;
     private List<String> correctTagList;
+
+    private Project project;
 
     @Rule
     public TemporaryFolder projectRootFolder = new TemporaryFolder();
@@ -52,33 +46,23 @@ public class SvnRepoManagerTest {
     @Rule
     public TemporaryFolder keyFolder = new TemporaryFolder();
 
-    @Autowired
-    private ProjectService projectService;
-
-    @Autowired
-    private ProjectgroupService projectgroupService;
-
     @Before
-    public void setup() {
-        try {
-            repositoryFile = repositoryResource.getFile();
-            logger.info("Found testrepo at " + repositoryFile.getAbsolutePath());
-            pgid = projectgroupService.create("Test_Group").getId();
-            project = projectService.create(pgid, "file://" + repositoryFile.getAbsolutePath() + "/test_project/", "SVN");
-            svnRepoManager = new SvnRepoManager(project);
-            svnRepoManager.initRepoManager(keyFolder.getRoot(), projectRootFolder.getRoot());
-            // Branches
-            correctBranchList = new ArrayList<>();
-            correctBranchList.add("branch1");
-            correctBranchList.add("branch2");
-            correctBranchList.add("trunk");
-            // Tags
-            correctTagList = new ArrayList<>();
-            correctTagList.add("tag1");
-            correctTagList.add("tag2");
-        } catch (IOException ex) {
-            logger.error("Could not find test resource testrepo_svn!");
-        }
+    public void setup() throws IOException {
+        File repositoryFile = new ClassPathResource("testrepo_svn").getFile();
+        project = mock(Project.class);
+        when(project.getRepositoryUrl()).thenReturn("file://" + repositoryFile.getAbsolutePath() + "/test_project/");
+        logger.info("Found testrepo at " + repositoryFile.getAbsolutePath());
+        svnRepoManager = new SvnRepoManager(project);
+        svnRepoManager.initRepoManager(keyFolder.getRoot(), projectRootFolder.getRoot());
+        // Branches
+        correctBranchList = new ArrayList<>();
+        correctBranchList.add("branch1");
+        correctBranchList.add("branch2");
+        correctBranchList.add("trunk");
+        // Tags
+        correctTagList = new ArrayList<>();
+        correctTagList.add("tag1");
+        correctTagList.add("tag2");
     }
 
     @Test
@@ -106,10 +90,4 @@ public class SvnRepoManagerTest {
         assertThat(CollectionUtils.isEqualCollection(tagNames, correctTagList));
         assertThat(svnRepoManager.switchBranch("trunk")).isTrue();
     }
-
-    @After
-    public void cleanup() {
-        projectgroupService.deleteProjectgroup(pgid);
-    }
-
 }
