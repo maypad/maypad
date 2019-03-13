62418d364d3cbfa1ff16102ebac5a36ebb3daec9
Az0r <15131687+Az0r@users.noreply.github.com>
2019-03-13 17:51:00 +0100

    Add prefix to keys from config file to avoid conflicts

--- backend/src/main/java/de/fraunhofer/iosb/maypadbackend/config/server/YamlServerConfig.java
+++ backend/src/main/java/de/fraunhofer/iosb/maypadbackend/config/server/YamlServerConfig.java
@@ -12,6 +12,7 @@ import org.springframework.core.io.FileSystemResource;
 
 import javax.annotation.PostConstruct;
 import java.io.File;
+import java.util.Properties;
 
 /**
  * Serverconfiguration as YAML File.
@@ -22,43 +23,44 @@ public class YamlServerConfig implements ServerConfig {
 
     private static final Logger logger = LoggerFactory.getLogger(YamlServerConfig.class);
 
-    @Value("${MAYPAD_WEB_SERVER_PORT:${webServerPort:-1}}")
+    @Value("${MAYPAD_WEB_SERVER_PORT:${maypad.webServerPort:-1}}")
     private int webServerPort;
-    @Value("${MAYPAD_RELOAD_REPOSITORIES_SECONDS:${reloadRepositoriesSeconds:900}}")
+    @Value("${MAYPAD_RELOAD_REPOSITORIES_SECONDS:${maypad.reloadRepositoriesSeconds:900}}")
     private int reloadRepositoriesSeconds;
-    @Value("${MAYPAD_MAXIMUM_REFRESH_REQUESTS_ENABLED:${maximumRefreshRequests.enabled:0}}")
+    @Value("${MAYPAD_MAXIMUM_REFRESH_REQUESTS_ENABLED:${maypad.maximumRefreshRequests.enabled:0}}")
     private boolean maximumRefreshRequestsEnabled;
-    @Value("${MAYPAD_MAXIMUM_REFRESH_REQUESTS_SECONDS:${maximumRefreshRequests.seconds:900}}")
+    @Value("${MAYPAD_MAXIMUM_REFRESH_REQUESTS_SECONDS:${maypad.maximumRefreshRequests.seconds:900}}")
     private int maximumRefreshRequestsSeconds;
-    @Value("${MAYPAD_MAXIMUM_REFRESH_REQUESTS_MAXIMUM_REQUESTS:${maximumRefreshRequests.maximumRequests:100}}")
+    @Value("${MAYPAD_MAXIMUM_REFRESH_REQUESTS_MAXIMUM_REQUESTS:${maypad.maximumRefreshRequests.maximumRequests:100}}")
     private int maximumRefreshRequests;
-    @Value("${MAYPAD_LOG_LEVEL:${logLevel:INFO}}")
+    @Value("${MAYPAD_LOG_LEVEL:${maypad.logLevel:INFO}}")
     private String logLevel;
-    @Value("${MAYPAD_REPOSITORY_STORAGE_PATH:${repositoryStoragePath:not set}}")
+    @Value("${MAYPAD_REPOSITORY_STORAGE_PATH:${maypad.repositoryStoragePath:not set}}")
     private String repositoryStoragePath;
-    @Value("${MAYPAD_MYSQL_USER:${mysql.user:not set}}")
+    @Value("${MAYPAD_MYSQL_USER:${maypad.mysql.user:not set}}")
     private String dbUser;
-    @Value("${MAYPAD_MYSQL_PASSWORD:${mysql.password:not set}}")
+    @Value("${MAYPAD_MYSQL_PASSWORD:${maypad.mysql.password:not set}}")
     private String dbPassword;
-    @Value("${MAYPAD_MYSQL_DATABASE:${mysql.database:not set}}")
+    @Value("${MAYPAD_MYSQL_DATABASE:${maypad.mysql.database:not set}}")
     private String dbDatabase;
-    @Value("${MAYPAD_MYSQL_HOST:${mysql.host:not set}}")
+    @Value("${MAYPAD_MYSQL_HOST:${maypad.mysql.host:not set}}")
     private String dbHost;
-    @Value("${MAYPAD_MYSQL_PORT:${mysql.port:-1}}")
+    @Value("${MAYPAD_MYSQL_PORT:${maypad.mysql.port:-1}}")
     private int dbPort;
-    @Value("${MAYPAD_SCHEDULER_POOL_SIZE:${scheduler.poolSize:2}}")
+    @Value("${MAYPAD_SCHEDULER_POOL_SIZE:${maypad.scheduler.poolSize:2}}")
     private int schedulerPoolSize;
-    @Value("${MAYPAD_WEBHOOK_TOKEN_LENGTH:${webhook.tokenLength:20}}")
+    @Value("${MAYPAD_WEBHOOK_TOKEN_LENGTH:${maypad.webhook.tokenLength:20}}")
     private int webhookTokenLength;
-    @Value("${MAYPAD_DOMAIN:${domain:not set}}")
+    @Value("${MAYPAD_DOMAIN:${maypad.domain:not set}}")
     private String domain;
 
     @PostConstruct
     private void init() {
         logger.info("Active configuration:\n"
                     + "webServerPort: {}\n"
+                    + "domain: {}\n"
                     + "reloadRepositoriesSeconds: {}\n"
-                    + "scheduler.reloadRepositoriesSeconds: {}\n"
+                    + "scheduler.poolSize: {}\n"
                     + "webhook.tokenLength: {}\n"
                     + "maximumRefreshRequests.enabled: {}\n"
                     + "maximumRefreshRequests.seconds: {}\n"
@@ -70,7 +72,7 @@ public class YamlServerConfig implements ServerConfig {
                     + "mysql.database: {}\n"
                     + "mysql.host: {}\n"
                     + "mysql.port: {}",
-                    webServerPort, reloadRepositoriesSeconds, reloadRepositoriesSeconds, webhookTokenLength,
+                    webServerPort, domain, reloadRepositoriesSeconds, schedulerPoolSize, webhookTokenLength,
                     maximumRefreshRequestsEnabled, maximumRefreshRequestsSeconds, maximumRefreshRequests,
                     logLevel, repositoryStoragePath, dbUser, dbPassword, dbDatabase, dbHost, dbPort);
     }
@@ -82,8 +84,6 @@ public class YamlServerConfig implements ServerConfig {
      */
     @Bean
     public static PropertySourcesPlaceholderConfigurer properties() {
-        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer
-                = new PropertySourcesPlaceholderConfigurer();
         YamlPropertiesFactoryBean yaml = new YamlPropertiesFactoryBean();
         String maypadHomePath = System.getenv("MAYPAD_HOME");
         maypadHomePath = maypadHomePath == null ? "/usr/share/maypad" : maypadHomePath;
@@ -101,7 +101,17 @@ public class YamlServerConfig implements ServerConfig {
             logger.error("MAYPAD_HOME is not set properly.");
             throw new RuntimeException("MAYPAD_HOME ist not set properly");
         }
-        propertySourcesPlaceholderConfigurer.setProperties(yaml.getObject());
+
+        //Add "maypad" prefix to avoid conflicts with environment variables used by other applications.
+        Properties prefixedProperties = new Properties();
+        yaml.getObject().entrySet().forEach(e -> {
+            if (e.getKey() instanceof String) {
+                prefixedProperties.setProperty("maypad." + e.getKey(), e.getValue().toString());
+            }
+        });
+        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer
+                = new PropertySourcesPlaceholderConfigurer();
+        propertySourcesPlaceholderConfigurer.setProperties(prefixedProperties);
         return propertySourcesPlaceholderConfigurer;
     }
 }
